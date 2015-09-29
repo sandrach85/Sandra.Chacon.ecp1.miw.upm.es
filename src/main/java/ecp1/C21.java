8c8ee4fbde4073353dc200e4eecc75e7d4aeb538
Sandra <sandrachacon@alumnos.upm.es>
2015-09-29 19:22:38 +0200

    Create C21

---
+++ src/main/java/ecp1/C21.java
@@ -0,0 +1,14 @@
+package ecp1;
+
+public class C21 {
+    public String m1() {
+        String nombre = "m1";
+        return nombre;
+    }
+
+    public String m2() {
+        String nombre = "m2";
+        return nombre;
+    }
+
+}
